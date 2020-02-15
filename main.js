0a92893f01967ae6ba74edeec971599886a8e15e
Mayra-RR <maiira.regalado@gmail.com>
2020-02-15 15:33:23 -0600

    Classes_May

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*-----------------------------Class--------------------------------*/
+/*-----------------------------1 - Class--------------------------------*/
 /*
 Definir una clase llamada figura.
 esta clase debe tener dos propiedades:
@@ -10,12 +10,24 @@ tener una método llamda area que como su nombre lo dice calcule el área de la 
 
 /*-----------------------------Solución------------------------------*/
 
+  class figure {
+    constructor(width, height) {
+      this.width = width;
+      this.height = height;
+    }
+    area() {
+      return this.width * this.height;
+    }
+  }
 
+var figureF = new figure(10, 5);
+var figureFArea =  figureF.area();
+console.log("Ex.1 Figure: ");
+console.log("Area Figure:" + figureFArea);
 
 /*------------------------------------------------------------------*/
 
-
-/*-----------------------------Herencia--------------------------------*/
+/*-----------------------------2 - Herencia--------------------------------*/
 /*
 1.- Definir una clase llamada rectangulo que herede de la clase figura.
 *Hacer lo necesario para poder usar esta nueva clase para crear rectangulos
@@ -29,13 +41,29 @@ y porder calcular el area de estos usando métodos.
 
 /*-----------------------------Solución------------------------------*/
 //rectangulo
+class rectangle extends figure {
+    perimetro(){
+        return 2*(this.width + this.height)
+    }
+  }
+var rect = new rectangle(4, 9);
+console.log("Ex.2.1 Rectangle: ");
+console.log("Area Rec:" + rect.area(), 
+"Perimetro Rec:" + rect.perimetro());
 
 //triangulo
-
+class triangle extends figure {
+  area() {
+    return (this.width * this.height) / 2;
+  }
+}
+var trian = new triangle(5, 10);
+console.log("Ex.2.2 Triangle: ");
+console.log("Area Triangle:" + trian.area());
 
 /*------------------------------------------------------------------*/
 
-/*-----------------------------Static--------------------------------*/
+/*-----------------------------3 - Static--------------------------------*/
 /*
 Definir una clase llamada trigonometría (o similar.)
 Esta clase debe ser diseñada teniendo en cuenta que no se generaran
@@ -53,12 +81,19 @@ en un escenario del mundo real.
 
 /*-----------------------------Solución------------------------------*/
 
+class trigonometria {
+  static hypotenuse(obj) {
+    return Math.hypot(obj.width, obj.height);
+  }
+  static rel = "This is the hypotenuse"
+}
+console.log("Ex.3 Trigonometry: ");
 
-
+console.log(trigonometria.rel);
+console.log(trigonometria.hypotenuse(trian));
 /*------------------------------------------------------------------*/
 
-
-/*-----------------------------Extend--------------------------------*/
+/*-----------------------------4 - Extend--------------------------------*/
 /*
 Tomando en cuenta los built-in objects vistos en la clase anterior hacer
 lo siguiente:
@@ -73,8 +108,20 @@ clase hija, en otras palabras deben de ser funcionando como hasta ahora.
 */
 
 /*-----------------------------Solución------------------------------*/
-
-
+let str = "pepe pecas pica papas con un pico";
+class enhancedString extends String {
+  puntuacion(str) {
+    return str + ".";
+  }
+  capitalizar(str) {
+    let min = str[0].toUpperCase();
+    return str.replace(str[0], min);
+  }
+}
+var cadena = new enhancedString();
+console.log("Ex.4 Extend: ");
+console.log("Capitalizar: " + cadena.capitalizar(str));
+console.log("Punto: " + cadena.puntuacion(str));
 
 /*------------------------------------------------------------------*/
 
@@ -96,7 +143,5 @@ hacer una de las siguientes opciones:
 Una vez definidos todo esto, favor de generar el código de las funciones.
 
 /*-----------------------------Solución------------------------------*/
-
-
 
 /*------------------------------------------------------------------*/
